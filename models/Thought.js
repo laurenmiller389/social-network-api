dea4c0a994a4a4953111890ae9c7e05f7b7797cf
Lauren Miller <lmiller90@fordham.edu>
2023-07-14 17:54:07 -0400

    completed Thought model

---
+++ models/Thought.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      //need that getter method
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Reaction',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+const Thought = model('thought', thoughtSchema);
+
+//make the reaction count schema with a virtual for its length
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length
+})
+
+module.exports = Thought;
