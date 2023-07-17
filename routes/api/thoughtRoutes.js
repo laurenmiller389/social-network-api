2127234935bee638f89a0b6255c5f9e07ccf173e
Lauren Miller <lmiller90@fordham.edu>
2023-07-16 22:09:48 -0400

    fixed thought route

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -12,13 +12,13 @@ const {
 // /api/thoughts
 router.route('/').get(getThoughts).post(createThought);
 
-// /api/courses/:courseId
+// /api/thoughts/:thoughtId
 router
   .route('/:thoughtId')
   .get(getSingleThought)
   .put(updateThought)
   .delete(deleteThought)
-  .create(createReaction)
+  .post(createReaction)
   .delete(deleteReaction)
 module.exports = router;
 
